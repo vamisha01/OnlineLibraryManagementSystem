772b31597c7c05981182ff5f8f45251ed6be03a6
vamisha01 <amishaupadhyay02@gmail.com>
2025-06-14 18:53:03 +0530

    Create Book.java

---
+++ src/model/Book.java
@@ -0,0 +1,17 @@
+package library.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(int id, String title, String author, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // Getters and Setters
+}
