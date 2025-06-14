899f5c889b569845d799c6dc2c354aefebe13075
vamisha01 <amishaupadhyay02@gmail.com>
2025-06-14 18:54:16 +0530

    Create student.java

---
+++ src/model/student.java
@@ -0,0 +1,15 @@
+package library.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+
+    public Student(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and Setters
+}
