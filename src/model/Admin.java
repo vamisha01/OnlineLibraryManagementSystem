ba33b60f72fd3380e83a7eab80af762c4ef8f8cd
vamisha01 <amishaupadhyay02@gmail.com>
2025-06-14 18:55:41 +0530

    Create Admin.java

---
+++ src/model/Admin.java
@@ -0,0 +1,13 @@
+package library.model;
+
+public class Admin {
+    private String username;
+    private String password;
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+}
