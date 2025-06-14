1dabd47754a1e3b42ddc8635ea58bc4f46dd3592
vamisha01 <amishaupadhyay02@gmail.com>
2025-06-14 18:59:20 +0530

    Create IssueLogPanel.java

---
+++ src/model/ui/IssueLogPanel.java
@@ -0,0 +1,9 @@
+package library.ui;
+
+import javax.swing.*;
+
+public class IssueLogPanel extends JPanel {
+    public IssueLogPanel() {
+        add(new JLabel("Manage Issue Logs Here"));
+    }
+}
